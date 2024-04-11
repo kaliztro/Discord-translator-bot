b51da61ec137a3cd9ece93a65f075e876f7c94e6
Kaliztro <kaliztro.s@gmail.com>
2024-04-11 19:57:36 -0300

    adicionado escolhas de idioma para traduzir

--- src/interactions/slashCommands/translator.js
+++ src/interactions/slashCommands/translator.js
@@ -16,50 +16,73 @@ export const Slash = {
             name: "traduzir_para",
             description: "Digite para qual língua deseja traduzir",
             type: 3,
-            required: false
+            required: false,
+            choices: [
+                { name: "English", value: "en" },
+                { name: "Russian", value: "ru" },
+                { name: "Spanish", value: "es" },
+                { name: "French", value: "fr" },
+                { name: "German", value: "de" },
+                { name: "Portuguese", value: "pt" },
+                { name: "Chinese", value: "zh" },
+                { name: "Japanese", value: "ja" },
+                { name: "Arabic", value: "ar" },
+                { name: "Italian", value: "it" },
+                { name: "Dutch", value: "nl" },
+                { name: "Korean", value: "ko" },
+                { name: "Swedish", value: "sv" },
+                { name: "Turkish", value: "tr" },
+                { name: "Polish", value: "pl" },
+                { name: "Indonesian", value: "id" },
+                { name: "Greek", value: "el" },
+                { name: "Danish", value: "da" },
+                { name: "Norwegian", value: "no" },
+            ]
         }
-    ],    
+    ],
     run: async (interaction) => {
         const texto = interaction.options.getString('texto');
-        const target = interaction.options.getString('traduzir_para');
-        const traduzirPara = target ? target : 'pt-br';
+        const target = interaction.options.get('traduzir_para');
+
+        let targetValidator = target === null ? 'pt-br' : target.value;
+
+        const traduzirPara = targetValidator;
 
         if (texto.length > 4096) {  //limite api 5000, limite do discord field 1024; description 4096
             return interaction.reply({ content: 'Oops, a mensagem é muito longa para traduzir. \n O máximo de caracteres que posso enviar é 4096, tente traduzir o texto em partes usando o comando /traduzir', ephemeral: true });
         }
 
         const regiao = {
-            [`en`]: `:flag_us: EUA`, 
-            [`de`]: `:flag_de: Deutsch `, 
-            [`es`]: `:flag_es: Español `, 
-            [`fr`]: `:flag_fr: Français `, 
-            [`hr`]: `:flag_hr: Hrvatski `, 
-            [`it`]: `:flag_it: Italiano `, 
-            [`pl`]: `:flag_pl: Polski `, 
-            [`ro`]: `:flag_ro: Româna `, 
-            [`vi`]: `:flag_vi: Tieng Viet `, 
+            [`en`]: `:flag_us: EUA`,
+            [`de`]: `:flag_de: Deutsch `,
+            [`es`]: `:flag_es: Español `,
+            [`fr`]: `:flag_fr: Français `,
+            [`hr`]: `:flag_hr: Hrvatski `,
+            [`it`]: `:flag_it: Italiano `,
+            [`pl`]: `:flag_pl: Polski `,
+            [`ro`]: `:flag_ro: Româna `,
+            [`vi`]: `:flag_vi: Tieng Viet `,
             [`cs`]: `:flag_cz: Cestina `,
-            [`pt`]: `:flag_br: Brasil `, 
-            [`da`]: `:flag_dk: Dansk `, 
-            [`lt`]: `:flag_lt: lietuviskai `, 
-            [`hu`]: `:flag_hu: Magyar `, 
-            [`nl`]: `:flag_nl: Nederlands `, 
-            [`no`]: `:flag_no: Norsk `, 
-            [`fi`]: `:flag_fi: Suomi `, 
-            [`sv-SE`]: `:flag_se: Svenska `, 
-            [`tr`]: `:flag_tr: Turkçe `, 
+            [`pt`]: `:flag_br: Brasil `,
+            [`da`]: `:flag_dk: Dansk `,
+            [`lt`]: `:flag_lt: lietuviskai `,
+            [`hu`]: `:flag_hu: Magyar `,
+            [`nl`]: `:flag_nl: Nederlands `,
+            [`no`]: `:flag_no: Norsk `,
+            [`fi`]: `:flag_fi: Suomi `,
+            [`sv`]: `:flag_se: Svenska `,
+            [`tr`]: `:flag_tr: Turkçe `,
             [`el`]: `:flag_gr: Ελληνικά `,
-            [`bg`]: `:flag_bg: български `, 
-            [`ru`]: `:flag_ru: Русский `, 
-            [`uk`]: `:flag_ua: Украïнська `, 
-            [`hi`]: `:flag_in: हिंदी `, 
-            [`th`]: `:flag_th: ไทย `, 
-            [`zh-CN`]: `:flag_cn: 中文 `, 
-            [`ja`]: `:flag_jp: 日本語 `, 
-            [`zh-TW`]: `:flag_tw: 繁體 中文 `, 
+            [`bg`]: `:flag_bg: български `,
+            [`ru`]: `:flag_ru: Русский `,
+            [`uk`]: `:flag_ua: Украïнська `,
+            [`hi`]: `:flag_in: हिंदी `,
+            [`th`]: `:flag_th: ไทย `,
+            [`zh`]: `:flag_cn: 中文 `,
+            [`ja`]: `:flag_jp: 日本語 `,
             [`ko`]: `:flag_kr: 한국어 `,
         };
-        
+
 
         try {
             // Detectar o idioma do texto
